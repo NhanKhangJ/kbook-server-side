1e8b347558af99cc852fa9fbe8185fbdb8ed156f
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2023-01-11 00:04:31 -0700

    userSchema structure update, ready for updateUserRoute

--- models/user.js
+++ models/user.js
@@ -4,7 +4,16 @@ const userSchema = mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
-    id:{type:String}
+    id:{type:String},
+    avatar:{type:String},
+    cover:{type:String},
+    job:{type:String},
+    education:{type:String},
+    location:{type:String},
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
 })
 
 const kbookUser = mongoose.model('kbookUser', userSchema);
