765923dab9dde101f62de305bbda5d524d259cc5
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2023-01-17 00:02:52 -0700

    fix token split because of delayed

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken';
 
 const auth = async(req,res,next) =>{
     try {
-        const token = req.headers.authorization.split(" ")[1];
-
-        const isCustomAuth = token.length < 500;
+        const token = req?.headers?.authorization?.split(" ")[1];
+        // console.log({token}) 
+        const isCustomAuth = token?.length < 500;
 
         let decodedData;
        
